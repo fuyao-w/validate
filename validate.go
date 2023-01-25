46c1e614c3cfb7b06409b30cd6e1457da20acb64
wangfuyao <wangfuyao@bytedance.com>
2023-01-25 21:27:00 +0800

    init

---
+++ validate.go
@@ -0,0 +1,339 @@
+package validate
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type (
+	compare interface {
+		lt(control, target string) bool
+		le(control, target string) bool
+		ge(control, target string) bool
+		gt(control, target string) bool
+	}
+	numCompare  struct{}
+	timeCompare struct{}
+)
+
+var errParseFail = errors.New("parse string fail")
+
+func toInt64(val string) int64 {
+	res, _ := strconv.ParseInt(val, 10, 64)
+	return res
+}
+func toUint64(val string) (uint64, error) {
+	return strconv.ParseUint(val, 10, 64)
+}
+func toFloat64(val string) (float64, error) {
+	return strconv.ParseFloat(val, 64)
+}
+
+var (
+	numReg = regexp.MustCompile(`\d+`)
+)
+
+// parseTimeDuration 将时间字符串解析成 数字+单位
+func parseTimeDuration(val string) (num int64, unit string) {
+	numStr := numReg.FindString(val)
+	num, err := strconv.ParseInt(numStr, 10, 61)
+	if err != nil {
+		panic(fmt.Errorf("parseTimeDuration err :%s", err))
+	}
+	return num, val[len(numStr):]
+}
+
+// toTimeDuration 将时间字符串转换成 time.Duration
+func toTimeDuration(val string) time.Duration {
+	var (
+		num, unit = parseTimeDuration(val)
+	)
+
+	return time.Duration(num) * func() time.Duration {
+		switch unit {
+		case "milli":
+			return time.Millisecond
+		case "m":
+			return time.Minute
+		case "h":
+			return time.Hour
+		case "d":
+			return time.Hour * 24
+		default:
+			return time.Duration(1)
+		}
+	}()
+}
+
+func (t timeCompare) lt(control, target string) bool {
+	return toTimeDuration(control) < toTimeDuration(target)
+}
+
+func (t timeCompare) le(control, target string) bool {
+	return toTimeDuration(control) <= toTimeDuration(target)
+}
+
+func (t timeCompare) ge(control, target string) bool {
+
+	return toTimeDuration(control) >= toTimeDuration(target)
+}
+
+func (t timeCompare) gt(control, target string) bool {
+
+	return toTimeDuration(control) > toTimeDuration(target)
+}
+
+func getUintNum(control, target string) (c, t uint64, err error) {
+	c, cerr := toUint64(control)
+	t, terr := toUint64(target)
+	if cerr != nil || terr != nil {
+		return 0, 0, errParseFail
+	}
+	return c, t, err
+}
+func getFloatNum(control, target string) (c, t float64, err error) {
+	c, cerr := toFloat64(control)
+	t, terr := toFloat64(target)
+	if cerr != nil || terr != nil {
+		return 0, 0, errParseFail
+	}
+	return c, t, err
+}
+func (n numCompare) lt(control, target string) bool {
+	if c, t, err := getUintNum(control, target); err == nil {
+		return c < t
+	}
+	if c, t, err := getFloatNum(control, target); err == nil {
+		return c < t
+	}
+
+	return toInt64(control) < toInt64(target)
+}
+
+func (n numCompare) le(control, target string) bool {
+	if c, t, err := getUintNum(control, target); err == nil {
+		return c <= t
+	}
+	if c, t, err := getFloatNum(control, target); err == nil {
+		return c <= t
+	}
+	return toInt64(control) <= toInt64(target)
+}
+
+func (n numCompare) ge(control, target string) bool {
+	if c, t, err := getUintNum(control, target); err == nil {
+		return c >= t
+	}
+	if c, t, err := getFloatNum(control, target); err == nil {
+		return c >= t
+	}
+	return toInt64(control) >= toInt64(target)
+}
+
+func (n numCompare) gt(control, target string) bool {
+	if c, t, err := getUintNum(control, target); err == nil {
+		return c > t
+	}
+	if c, t, err := getFloatNum(control, target); err == nil {
+		return c > t
+	}
+	return toInt64(control) > toInt64(target)
+}
+
+const (
+	timeRegexp = `^[[(](\d+([mdh]|milli)){1},(\d+([mdh]|milli)){1}[])]$`
+	numRegexp  = `^[[(]([+-]?\d+)|~{1},([+-]?\d+)|~{1}[])]$`
+	selfRegexp = `self\.\w+`
+)
+
+var regMap = map[string]*regexp.Regexp{
+	timeRegexp: regexp.MustCompile(timeRegexp),
+	numRegexp:  regexp.MustCompile(numRegexp),
+	selfRegexp: regexp.MustCompile(selfRegexp),
+}
+
+func checkTagValidate(exp string, val string) {
+	//根据规则提取关键信息
+	if len(regMap[exp].FindString(val)) == 0 {
+		panic(fmt.Errorf("validate tag :%s", val))
+	}
+}
+
+type checkFunc func(field, val, control string) error
+
+type direction int
+
+const (
+	left direction = iota + 1
+	right
+)
+
+func parsePartition(direction direction, val string, comp compare, control string) (success bool, _ string) {
+	var rangeKey, target string
+	switch direction {
+	case left:
+		rangeKey, target = val[0:1], val[1:]
+	case right:
+		rangeKey, target = val[len(val)-1:], val[:len(val)-1]
+	}
+	if target == "~" {
+		return true, ""
+	}
+	switch rangeKey {
+	case "[":
+		return comp.ge(control, target), "too low"
+	case "(":
+		return comp.gt(control, target), "too low"
+	case "]":
+		return comp.le(control, target), "too high"
+	case ")":
+		return comp.lt(control, target), "too high"
+	default:
+		panic("illegal border")
+	}
+	return
+}
+
+func builder(comp compare) checkFunc {
+	return func(field, val, control string) error {
+		valid := func(direction direction, singlePartition string) error {
+			if succ, hint := parsePartition(direction, singlePartition, comp, control); !succ {
+				return fmt.Errorf("validate fail: %s is %s", field, hint)
+			}
+			return nil
+		}
+		partition := strings.Split(val, ",")
+		leftPar, rightPar := partition[0], partition[1]
+		if err := valid(left, leftPar); err != nil {
+			return err
+		}
+		if err := valid(right, rightPar); err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+// replaceSelfExp 将 self.{字段名} 替换成具体值
+func replaceSelfExp(str string, t reflect.Type, v reflect.Value) string {
+	list := regMap[selfRegexp].FindAllString(str, -1)
+	if len(list) == 0 {
+		return str
+	}
+
+	for _, s := range list {
+		name := strings.Trim(s, "self.")
+		_, ok := t.FieldByName(name)
+		if !ok {
+			panic(fmt.Errorf("field :%s not exist", name))
+		}
+
+		value := v.FieldByName(name)
+		if !value.IsValid() {
+			panic(fmt.Errorf("not found filed :%s", s))
+		}
+		if !value.CanInt() && !value.CanUint() {
+			panic(fmt.Errorf("filed :%s can't convert to int", s))
+		}
+		str = strings.Replace(str, s, number2Str(value), 1)
+	}
+	return str
+}
+
+func getTypeStruct(t reflect.Type) reflect.Type {
+	if t.Kind() != reflect.Struct && t.Kind() != reflect.Pointer {
+		panic("validate target must be struct")
+	}
+	if t.Kind() == reflect.Struct {
+		return t
+	}
+	return getTypeStruct(t.Elem())
+}
+func getValueStruct(t reflect.Value) reflect.Value {
+	if t.Kind() != reflect.Struct && t.Kind() != reflect.Pointer {
+		panic("validate target must be struct")
+	}
+	if t.Kind() == reflect.Struct {
+		return t
+	}
+	return getValueStruct(t.Elem())
+}
+
+// Validate 校验结构体的字段是否合法
+/*
+- 使用 `valid` tag 进行标记
+- 对于数字类型使用范围校验，通过 [] () 代表边界 ，数字和可以带符号 [+、-] , 可以使用 ~ 代表无穷
+- 对于时间类型，范围校验与数组相同，需要在数字后面加上单位 [milli,m,h,d] ，不支持符号
+- 也可以支持与当前结构体的其他字段进行比较，通过 self.{字段名} 指定 ，注意 int 和 time.Duration 也可以进行比较
+*/
+func Validate(i interface{}) error {
+	iType := getTypeStruct(reflect.TypeOf(i))
+	iValue := getValueStruct(reflect.ValueOf(i))
+
+	for i := 0; i < iType.NumField(); i++ {
+		field := iType.Field(i)
+
+		valid := replaceSelfExp(field.Tag.Get("valid"), iType, iValue)
+		if valid == "" {
+			continue
+		}
+		var doCheck checkFunc
+		switch {
+		case isNumKind[field.Type.String()]:
+			checkTagValidate(numRegexp, valid)
+			doCheck = builder(numCompare{})
+		case field.Type.String() == "time.Duration":
+			checkTagValidate(timeRegexp, valid)
+			doCheck = builder(timeCompare{})
+		}
+
+		value := iValue.Field(i)
+		checkNumber(value, field.Name)
+
+		if err := doCheck(field.Name, valid, number2Str(value)); err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
+
+func checkNumber(value reflect.Value, name string) {
+	if !value.CanInt() && !value.CanUint() && !value.CanFloat() {
+		panic(fmt.Errorf("field :%s can't convert to number", name))
+	}
+}
+
+func number2Str(value reflect.Value) string {
+	if value.CanInt() {
+		return strconv.FormatInt(value.Int(), 10)
+	}
+	if value.CanUint() {
+		return strconv.FormatUint(value.Uint(), 10)
+	}
+	if value.CanFloat() {
+		return strconv.FormatFloat(value.Float(), 'e', 10, 64)
+	}
+	fmt.Println("sd")
+	panic("getInt err")
+}
+
+var isNumKind = map[string]bool{
+	"int":     true,
+	"int8":    true,
+	"int16":   true,
+	"int32":   true,
+	"int64":   true,
+	"uint":    true,
+	"uint8":   true,
+	"uint16":  true,
+	"uint32":  true,
+	"uint64":  true,
+	"float64": true,
+	"float32": true,
+}
---
+++ validate_test.go
@@ -0,0 +1,262 @@
+package validate
+
+import (
+	"math"
+	"reflect"
+	"regexp"
+	"strconv"
+	"testing"
+	"time"
+)
+
+type TestCast struct {
+	A int8          `valid:"[1,2]"`
+	B time.Duration `valid:"[1,2]"`
+}
+
+func TestName(t *testing.T) {
+	tc := TestCast{
+		A: 1,
+		B: time.Hour,
+	}
+	configType := reflect.TypeOf(tc)
+	if configType.Kind() != reflect.Struct {
+		panic("validate target must be struct")
+	}
+	configValue := reflect.ValueOf(tc)
+	for i := 0; i < configType.NumField(); i++ {
+		field := configType.Field(i)
+		valid := field.Tag.Get("valid")
+		if valid == "" {
+			continue
+		}
+		t.Log(field.Type.String())
+		value := configValue.Field(i)
+		t.Log(value.Int())
+	}
+}
+
+func TestReG(t *testing.T) {
+	type testCase struct {
+		str string
+		res bool
+	}
+	testCaseList := []testCase{
+		{
+			str: "[4milli,4milli]",
+			res: true,
+		},
+		{
+			str: "[1m,4m]",
+			res: true,
+		},
+
+		{
+			str: "[1h,4h]",
+			res: true,
+		},
+		{
+			str: "[1m,4d]",
+			res: true,
+		}, {
+			str: "[1d,4d]",
+			res: true,
+		}, {
+			str: "[1m,4d]",
+			res: true,
+		}, {
+			str: "[-1m,4d]",
+			res: false,
+		}, {
+			str: "[1m,+4d]",
+			res: false,
+		}, {
+			str: "[1,4day]",
+			res: false,
+		}, {
+			str: "[1milli,4d)",
+			res: true,
+		}, {
+			str: "-[1milli,4d)",
+			res: false,
+		}, {
+			str: "[1milli,4d)+",
+			res: false,
+		},
+	}
+
+	doCheck := func(exp string, testCaseList []testCase) {
+		reg1 := regexp.MustCompile(exp)
+		if reg1 == nil {
+			panic("regexp validate")
+		}
+		for _, c := range testCaseList {
+			//根据规则提取关键信息
+			result := reg1.FindString(c.str)
+			if len(result) > 0 != c.res {
+				t.Fail()
+				t.Logf("%s not true ,result :%v ", c.str, result)
+			}
+		}
+	}
+	doCheck(timeRegexp, testCaseList)
+	doCheck(numRegexp, []testCase{
+		{
+			str: "[1,3]",
+			res: true,
+		},
+		{
+			str: "[2,4)",
+			res: true,
+		}, {
+			str: "[-2,4)",
+			res: true,
+		}, {
+			str: "[-2,+4)",
+			res: true,
+		}, {
+			str: "-[-2,+4)",
+			res: false,
+		}, {
+			str: "-[2,4)",
+			res: false,
+		}, {
+			str: "-[2,4)+",
+			res: false,
+		}, {
+			str: "[-,4)",
+			res: false,
+		}, {
+			str: "[-,+)",
+			res: false,
+		}, {
+			str: "[-,+4)",
+			res: false,
+		},
+		{
+			str: "[~,+4)",
+			res: true,
+		}, {
+			str: "[4,~)",
+			res: true,
+		}, {
+			str: "[~,~)",
+			res: true,
+		}, {
+			str: "[~,~]",
+			res: true,
+		},
+	})
+
+}
+
+func TestTimeReg(t *testing.T) {
+	_ = `^[[(](\d+([mdh]|milli)){1},(\d+([mdh]|milli)){1}[])]$`
+	self := `self\.\w+`
+	reg1 := regexp.MustCompile(self)
+	if reg1 == nil {
+		panic("regexp validate")
+	}
+	result := reg1.FindAllString("[self.sDDDf,self.V]", -1)
+
+	t.Log(result, len(result))
+}
+
+func TestReplace(t *testing.T) {
+	type TestCase struct {
+		A int
+		B int
+	}
+	//v := reflect.ValueOf(TestCase{A: 1, B: 3})
+	//t.Log(replaceSelfExp("[self.A,self.V]", v))
+
+}
+
+func TestParsePartition(t *testing.T) {
+	t.Log(parsePartition(left, "[~", numCompare{}, "1"))
+	t.Log(parsePartition(left, "[1", numCompare{}, "1"))
+	t.Log(parsePartition(left, "(3", numCompare{}, "3"))
+	t.Log(parsePartition(right, "4]", numCompare{}, "1"))
+	t.Log(parsePartition(right, "3)", numCompare{}, "3"))
+	t.Log(parsePartition(right, "~]", numCompare{}, "3"))
+
+}
+
+func TestString(t *testing.T) {
+	right := "+33245]"
+	right = string(append([]byte{right[len(right)-1]}, right[:len(right)-1]...))
+	t.Log(right)
+}
+
+func TestGetStruct(t *testing.T) {
+	type X struct {
+	}
+	var x = &X{}
+
+	t.Log(getTypeStruct(reflect.TypeOf(x)).Kind())
+	t.Log(getValueStruct(reflect.ValueOf(x)).Kind())
+}
+
+func TestValidate(t *testing.T) {
+	type X struct {
+		c int32 `valid:"[1,3)"`
+		//a int    `valid:"[1,~]"`
+		b uint `valid:"[1,3]"`
+		//d uint64 `valid:"[0,18446744073709551615]"`
+		////e uint64 `valid:"[-3,18446744073709551615)"`
+		//f int64         `valid:"[~,234234)"`
+		//g time.Duration `valid:"[1milli,4m]"`
+		////h time.Duration `valid:"[1h,4m]"`
+		i int64         `valid:"[self.c,self.b]"`
+		j float64       `valid:"[1.3,1.9]"`
+		k time.Duration `valid:"[1m,5m]"`
+	}
+
+	var x = X{
+		//a: 9,
+		b: 3,
+		c: 2,
+		//d: 18446744073709551615,
+		////e: 18446744073709551615,
+		//f: 234234 - 1,
+		//g: 4 * time.Minute,
+		////h: time.Second,
+		//i: 3,
+		j: 1.3,
+	}
+
+	t.Log(Validate(&x))
+}
+
+func TestToTimeDuration(t *testing.T) {
+	t.Log(toTimeDuration("1min"))
+	t.Log(toTimeDuration("1day"))
+	t.Log(toTimeDuration("13milli"))
+	t.Log(toTimeDuration("13900milli"))
+}
+
+func TestParserTimeDur(t *testing.T) {
+	t.Log(parseTimeDuration("1m"))
+	t.Log(parseTimeDuration("133mi"))
+}
+
+func TestGetInt(t *testing.T) {
+	var i uint64 = math.MaxUint64
+	var b uint64 = math.MaxUint64
+	numCompare{}.lt(number2Str(reflect.ValueOf(i)), number2Str(reflect.ValueOf(b)))
+
+}
+
+func TestParseFloat(t *testing.T) {
+	t.Log(strconv.FormatFloat(3.1415937, 'e', 10, 64))
+
+}
+
+type Param struct {
+	A int           `valid:"[1,3)"`
+	B float64       `valid:"[~,3.3)"`
+	C uint64        `valid:"[2,45435]"`
+	D time.Duration `valid:"[500milli,3h]"`
+	E uint64
+	F uint64 `valid:"[self.C,self.E]"`
+}
